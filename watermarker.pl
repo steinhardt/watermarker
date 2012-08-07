d0c5133a51ececa352d08f0bf5ca9b613b09597b
Kevin Steinhardt <kevinsteinhardt@gmail.com>
2012-08-07 16:57:29 +0200

    Subroutine &watermark_image

--- watermarker.pl
+++ watermarker.pl
@@ -38,7 +38,7 @@ foreach $file (@files) { # TODO array @files should be stripped of any non-JPG f
 
 
 # checks and defines logo to be used for watermarking
-$logo_foo = "logo_100px.png"; # $logocheck so-named to avoid duplicate variables later on
+$logo_foo = "logo_100px.png"; # odd name avoids duplicate variables later on
 unless (-e $logo_foo) {
 	die "Cannot read file $logo_foo: $!\n";
 }
@@ -50,11 +50,10 @@ sub watermark_image {
 	my $logo = Image::Magick->new;  # "creates" new image object
 
 	$photo->Read($_[0]); # reads photograph from $file, passed to sub as $_[0]
+	$logo->Read($logo_foo); # reads logo from $logo_foo; defines it as $logo
 
-	$logo->Read($logocheck);
+	($width, $height) = $photo->Get('width','height'); # used later to determine image orientation
 
-	# determines image orientation (e.g. portrait) from dimensions
-	($width, $height) = $photo->Get('width','height');
 	print "Processing $_[0] (width $width, height $height)...\n";
 
 	# lays watermark over photograph
@@ -73,7 +72,7 @@ sub watermark_image {
 			gravity=>"South", y=>'25',
 		);
 	} else {
-		die "Dimensions didn't match. Must be 640 px wide or 480 px high.\n";
+		warn "Dimensions didn't match. Must be 640 px wide or 480 px high.\n";
 	}
 
 	$photo->Write("finalimage.jpg"); # TODO save watermarked image in watermarked/ dir, same name as source file - fail if file already exists.
