ff3d9a6edf85dc3b6fb7f4e093dd99634572134d
Kevin Steinhardt <kevinsteinhardt@gmail.com>
2012-08-07 16:45:26 +0200

    New subroutine &watermark_image

--- watermarker.pl
+++ watermarker.pl
@@ -31,59 +31,50 @@ if (-e $destination ) {
 
 # repeats subroutine for all files in $dir
 @files = <$dir>;
-foreach $file (@files) {
+foreach $file (@files) { # TODO array @files should be stripped of any non-JPG files. Error "Could not find any .jpg files in directory.\n" if no files after strip.
 	die "Noone's written this bit yet!\n";
 	# &watermark_image($file);
 }
 
 
-# defines new image objects
-my $photo = Image::Magick->new;
-my $logo = Image::Magick->new;
+# subroutine &watermark_image
+sub watermark_image {
+	my $photo = Image::Magick->new; # "creates" new image object
+	my $logo = Image::Magick->new;  # "creates" new image object
 
+	$photo->Read($_[0]); # reads photograph from $file, passed to sub as $_[0]
 
-# imports photograph and logo
-$photo->Read($ARGV[0]);
+	$logocheck = "logo_100px.png"; # $logocheck so-named to avoid duplicate variables later on
+	unless (-e $logocheck) {
+		die "Cannot read logo file.\n";
+	}
+	$logo->Read($logocheck);
 
-$logocheck = "logo_100px.png"; # $logocheck so-named to avoid duplicate variables later on
-unless (-e $logocheck) {
-	die "Cannot read logo file.\n";
+	# determines image orientation (e.g. portrait) from dimensions
+	($width, $height) = $photo->Get('width','height');
+	print "Processing $_[0] (width $width, height $height)...\n";
+
+	# lays watermark over photograph
+	if ($width == "640") {
+		$photo->Composite (
+			image=>$logo,
+			compose=>'plus',
+			blend=>'50x50',
+			gravity=>"South", y=>'25',
+		);
+	} elsif ($height == "480") {
+		$photo->Composite (
+			image=>$logo,
+			compose=>'plus',
+			blend=>'50x50',
+			gravity=>"South", y=>'25',
+		);
+	} else {
+		die "Dimensions didn't match. Must be 640 px wide or 480 px high.\n";
+	}
+
+	$photo->Write("finalimage.jpg"); # TODO save watermarked image in watermarked/ dir, same name as source file - fail if file already exists.
 }
-$logo->Read($logocheck);
-
-
-# determines image orientation (e.g. portrait) from dimensions
-($width, $height) = $photo->Get('width','height');
-print "Processing $ARGV[0] (width $width, height $height)...\n";
-
-
-# lays watermark over photograph
-if ($width == "640") {
-	$photo->Composite (
-		image=>$logo,
-		compose=>'plus',
-		blend=>'50x50',
-		gravity=>"South", y=>'25',
-	);
-} elsif ($height == "480") {
-	$photo->Composite (
-		image=>$logo,
-		compose=>'plus',
-		blend=>'50x50',
-		gravity=>"South", y=>'25',
-	);
-} else {
-	die "Dimensions didn't match. Must be 640 px wide or 480 px high.\n";
-}
-
-
-# writes watermarked image
-$photo->Write("finalimage.jpg");
 
 
 exit;
-
-
-# TODO Make dir `watermarked` in source dir if it doesn't exist already.
-# TODO Save watermarked image with same name as source in new dir `watermarked` - fail if file already exists.
-# TODO array @files should be stripped of any non-JPG files. Error "Could not find any .jpg files in directory.\n" if no files after strip.
